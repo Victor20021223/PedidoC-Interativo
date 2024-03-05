326691a1270edc17799b91f7c2ae302910625634
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2024-03-04 22:33:01 -0300

    feat: implementações tela de listagem de produtos

---
+++ UMFG.Venda.Aprensetacao/Models/ProdutoModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using UMFG.Venda.Aprensetacao.Classes;
+
+namespace UMFG.Venda.Aprensetacao.Models
+{
+
+   internal sealed class ProdutoModel : AbstractModel
+    {
+
+        private ImageSource _imagem;
+        private string _referencia;
+        private string _descricao;
+        private decimal _preco;
+
+    }
+}
